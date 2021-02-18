76eb7639f44a0b93ca3856dddd1b5d9b66b45e83
navdeep2008 <74094165+navdeep2008@users.noreply.github.com>
2021-02-18 11:44:42 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,40 +1,95 @@
-var wall , car;
-var speed , weight;
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
 
 function setup() {
-  createCanvas(800,400);
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
 
-  thickness = random(22,83);
-  speed = random(223,321);
-  weight = random(30,52);
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
 
-  car = createSprite(50,200, thickness, height/2);
-  car.velocityX = speed;
-  car.shapeColor = "black";
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
 
-  wall = createSprite(1200 , 200,60,height/2);
-  wall.shapeColor = "black";
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.4, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+ 	boxPosition=width/2-100
+ 	boxY=610;
+
+
+ 	boxleftSprite=createSprite(boxPosition, boxY, 20,100);
+ 	boxleftSprite.shapeColor=color(255,0,0);
+
+ 	boxLeftBody = Bodies.rectangle(boxPosition+20, boxY, 20,100 , {isStatic:true} );
+ 	World.add(world, boxLeftBody);
+
+ 	boxBase=createSprite(boxPosition+100, boxY+40, 200,20);
+ 	boxBase.shapeColor=color(255,0,0);
+
+ 	boxBottomBody = Bodies.rectangle(boxPosition+100, boxY+45-20, 200,20 , {isStatic:true} );
+ 	World.add(world, boxBottomBody);
+
+ 	boxleftSprite=createSprite(boxPosition+200 , boxY, 20,100);
+ 	boxleftSprite.shapeColor=color(255,0,0);
+
+ 	boxRightBody = Bodies.rectangle(boxPosition+200-20 , boxY, 20,100 , {isStatic:true} );
+ 	World.add(world, boxRightBody);
+
+
+	Engine.run(engine);
+  
 }
+
 
 function draw() {
-  background("black");  
-  var deformation = 0.5*bulletWeight*bulletSpeed*bulletSpeed/thicknessOfWall*thicknessOfWall*thicknessOfWall ;
+  rectMode(CENTER);
+  background(0);
+ 
+  packageSprite.x= packageBody.position.x;
+  packageSprite.y= packageBody.position.y;
   
-  if(wall.x - car.x <=car.width/2 + wall.width/2){
-  car.velocityX = 0;  
-  var deformation= 0.5 * weight * speed * speed/22500;
+  function keyPressed() {
+	if (keyCode === DOWN_ARROW) {
+   Matter.Body.setStatic(packageBody,false);
+   }
+   
+   if (keyCode === RIGHT_ARROW) {
+	   helicopterSprite.x = helicopterSprite.x+2;
+	   var translation = {x:20,y:0}
+	   Matter.Body.translate(packageBody, translation);  
+   }
+   
+   if (keyCode === LEFT_ARROW) {
+	   helicopterSprite.x = helicopterSprite.x-2;
+	   var translation = {x:-20,y:0}
+	   Matter.Body.translate(packageBody, translation)
+   }
   
-  if(deformation > 180){
-  car.shapeColor = color(255 , 0 , 0);  
-  }
-
-  if (deformation < 180 && deformation >  100){
-  car.shapeColor = color(230 ,230 ,0);  
-  }
-
-  if(deformation < 100){
-  car.shapeColor = color(0 , 255 , 0);  
-  }
-} 
   drawSprites();
-}
+}}
