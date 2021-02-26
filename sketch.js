b455e574d64669b1ef63a4270ec565628a901ce5
navdeep2008 <74094165+navdeep2008@users.noreply.github.com>
2021-02-26 11:11:42 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -22,7 +22,7 @@ function draw() {
   if(hasCollided(bullet , wall))
 {
 bullet.velocityX = 0;
-var deformation =  0.5*bulletWeight*bulletSpeed*bulletSpeed/(thickness*thickness*thickness);
+var damage =  0.5*bulletWeight*bulletSpeed*bulletSpeed/(thickness*thickness*thickness);
 
 if(damage > 10){
 wall.shapeColor = color(255,0,0);;  
